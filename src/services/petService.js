9fc612847a6cede90a7261a46a84c0a8f180c8f8
sourabh <85352464+sourabh707@users.noreply.github.com>
2024-06-27 17:59:07 +0530

    updated petService

--- src/services/petService.js
+++ src/services/petService.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export const fetchPets = async () => {
   try {
-    const response = await axios.get('https://run.mocky.io/v3/9e75b547-fd0e-4a1c-b40c-aa4fc943892f')
-    // console.log(response.data)
+    const response = await axios.get('https://run.mocky.io/v3/38fb30e5-437a-435e-af31-fcb27504d4c2')
+    console.log(response.data)
     return response?.data?.pets;
   } catch (error) {
-    // console.error('Error fetching pet data:', error);
+    console.error('Error fetching pet data:', error);
     return { error: error.message };
   }
 };
